f3dad8cf49138a9287561739e21f9f5be53a4886
Ryo Togashi <togashi@tunn-el.com>
2016-02-27 21:21:53 +0900

    リクエスト一覧画面Toolbarをいじってみるがうまくいかぬ

--- app/src/main/java/com/tjj/qq/InputTabActivity.java
+++ app/src/main/java/com/tjj/qq/InputTabActivity.java
@@ -77,6 +77,7 @@ public class InputTabActivity extends AppCompatActivity {
     }
 
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/com/tjj/qq/RequestListActivity.java
+++ app/src/main/java/com/tjj/qq/RequestListActivity.java
@@ -1,7 +1,9 @@
 package com.tjj.qq;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class RequestListActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class RequestListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.request_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.request_list_toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_request_list, menu);
+        return true;
+    }
+
+//    @Override
+//    public boolean onMenuOpened(int featureId, Menu menu) {
+//        Intent intent = new Intent(this, InputTabActivity.class);
+//        startActivity(intent);
+//        return true;
+//    }
 }
